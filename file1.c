3617ab4851e9400df0811d095b7b20557814c96a
snehajaiswal052@gmail.com <snehajaiswal052@gmail.com>
2024-04-29 00:56:36 +0530

    adding another program in main branch1

---
+++ file1.c
@@ -0,0 +1,12 @@
+//Another program to check if number is even or odd
+
+#include <stdio.h>
+int main() {
+	printf("enter a number");
+	scanf("%d", &num);
+        if (num%2==0) {
+		printf("%d is an even number\n");
+	} else {
+		printf("%d is an odd number\n");
+	}
+
